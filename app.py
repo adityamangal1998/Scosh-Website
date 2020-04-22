f6fd94bcc8ca108e01a8ec017d0d41afda854426
Aditya Mangal <adityamangalabc123@gmail.com>
2020-04-22 04:49:11 -0800

    add flask app

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+
+
+
+app = Flask(__name__)
+
+@app.route('/index')
+def home():
+    return render_template("index.html")
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/archive')
+def archive():
+    return render_template("archive.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+@app.route('/single')
+def single():
+    return render_template("single.html")
+
+@app.route('/static')
+def stat():
+    return render_template("static.html")
+
+
+
+app.run(debug=True)
